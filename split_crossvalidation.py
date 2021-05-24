ce693c45fca121af01c8baa8a7049c1ecd930367
ejawany95 <59008243+ejawany95@users.noreply.github.com>
2021-05-24 03:13:20 +0000

    Update split_crossvalidation.py

--- split_crossvalidation.py
+++ split_crossvalidation.py
@@ -2,8 +2,8 @@ import os
 from sklearn.model_selection import StratifiedKFold
 import numpy as np
 
-root = '/media/data1/suriza/MICCAI_BraTS2020_TrainingData_2'
-# valid_data_dir = '/media/data1/suriza/research/BraTS-DMFNet/MICCAI_BraTS_2018_Data_Validation'
+root = '' #path to training dataset directory
+
 
 def write(data, fname, root=root):
     fname = os.path.join(root, fname)
@@ -29,8 +29,8 @@ for k, (train_index, valid_index) in enumerate(skf.split(Y, Y)):
     train_list = list(X[train_index])
     valid_list = list(X[valid_index])
 
-    write(train_list, 'train_20_{}.txt'.format(k))
-    write(valid_list, 'valid_20_{}.txt'.format(k))
+    write(train_list, 'train_18_{}.txt'.format(k))
+    write(valid_list, 'valid_18_{}.txt'.format(k))
 
 # valid = os.listdir(os.path.join(valid_data_dir))
 # valid = [f for f in valid if not (f.endswith('.csv') or f.endswith('.txt'))]
